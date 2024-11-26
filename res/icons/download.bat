548f90b4e2c7ac563f5a127f327768d8e6bb0312
闪电炫芬批 <liuljwtt@163.com>
2024-11-26 18:34:21 +0800

    修正一下Git用户名，并更新了图标下载脚本，嘿嘿

--- res/icons/download.bat
+++ res/icons/download.bat
@@ -5,11 +5,16 @@ set CURL_HEADERS=-H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) Apple
 set CURL_HEADERS=%CURL_HEADERS% -H "Accept-Encoding: mage/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8"
 set CURL_HEADERS=%CURL_HEADERS% -H "Accept-Encoding: gzip, deflate, br, zstd"
 set CURL_HEADERS=%CURL_HEADERS% -H "Accept-Language: en,zh-CN;q=0.9,zh;q=0.8,en-GB;q=0.7,en-US;q=0.6"
-if not defined BHB_COOKIE (
-    set /p BHB_COOKIE=Enter your BHB cookie here: 
+@REM if not defined BHB_COOKIE (
+@REM     set /p BHB_COOKIE=Enter your BHB cookie here: 
+@REM )
+@REM set CURL_HEADERS=%CURL_HEADERS% -H "Cookie: %BHB_COOKIE%"
+@REM set PROXY_CONTENT=
+if defined PROXY_CONTENT (
+    set CURL_OPTIONS=%CURL_HEADERS% --proxy %PROXY_CONTENT%
+) else (
+    set CURL_OPTIONS=%CURL_HEADERS%
 )
-set CURL_HEADERS=%CURL_HEADERS% -H "Cookie: %BHB_COOKIE%"
 
-
-curl %CURL_HEADERS% -O https://boyshelpboys.com/upload/avatar/000/1.png?1730880348
+curl %CURL_OPTIONS% -O https://boyshelpboys.com/upload/avatar/000/1.png?1730880348
 move 1.png cached\avatar\1.png
