29943214d0be8402b4f531b278bacf68de0d324c
闪电炫芬批 <liuljwtt@163.com>
2024-11-28 09:50:03 +0800

    为火狐样式做优化，为打包做优化

--- pack.bat
+++ pack.bat
@@ -1,11 +1,15 @@
 if not exist ".\build\BHBChatRoomBG" mkdir ".\build\BHBChatRoomBG"
-if not exist ".\build\BHBChatRoomBG" goto:eof
+if not exist ".\build\BHBChatRoomBG" exit /B 1
 if exist ".\build\BHBChatRoomBG.xpi" del ".\build\BHBChatRoomBG.xpi"
-if exist ".\build\BHBChatRoomBG.xpi" goto:eof
+@ if exist ".\build\BHBChatRoomBG.xpi" @(
+    echo "Error: Fail to delete BHBChatRoomBG.xpi"
+    choice /M "Do you want to continue packing for Chrome/Edge"
+    if ERRORLEVEL 2 exit /B 1
+)
 del ".\build\BHBChatRoomBG\*.*" /Q /S
 rmdir ".\build\BHBChatRoomBG" /S /Q
 if not exist ".\build\BHBChatRoomBG\res\icons" mkdir ".\build\BHBChatRoomBG\res\icons"
-if not exist ".\build\BHBChatRoomBG\res\icons" goto:eof
+if not exist ".\build\BHBChatRoomBG\res\icons" exit /B 1
 xcopy /E /Y ".\src\*" ".\build\BHBChatRoomBG\"
 copy ".\res\icons\icon.png" ".\build\BHBChatRoomBG\res\icons\icon.png"
 copy ".\res\icons\lamp_121067.svg" ".\build\BHBChatRoomBG\res\icons\lamp_121067.svg"
